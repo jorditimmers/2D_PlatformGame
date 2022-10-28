1ac5b379594111a155afb361619ca325194ff0d1
Jordi Timmers <timmersjordi@outlook.com>
2022-10-28 18:26:59 +0200

    animation refactor

--- 2DPlatformGame/2DPlatformGame/Animations/Animation.cs
+++ 2DPlatformGame/2DPlatformGame/Animations/Animation.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Microsoft.Xna.Framework;
 
 namespace DPlatformGame.Animations
 {
@@ -27,6 +28,20 @@ namespace DPlatformGame.Animations
             if(counter >= frames.Count)
             {
                 counter = 0;
+            }
+        }
+
+        public void GetFramesFromTextureProperties(int width, int height, int numberOfWidthSprites, int numberOfHeightSprites)
+        {
+            int widthOfFrame = width / numberOfWidthSprites;
+            int heightOfFrame = height / numberOfHeightSprites;
+
+            for (int y = 0; y <= height - heightOfFrame; y += heightOfFrame)
+            {
+                for (int x = 0; x <= width - widthOfFrame; x += widthOfFrame)
+                {
+                    frames.Add(new AnimationFrame(new Rectangle(x, y, widthOfFrame, heightOfFrame)));
+                }
             }
         }
     }
--- 2DPlatformGame/2DPlatformGame/Characters/Samurai.cs
+++ 2DPlatformGame/2DPlatformGame/Characters/Samurai.cs
@@ -2,34 +2,31 @@
 using DPlatformGame.Interfaces;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using DPlatformGame.Animations;
 
 namespace DPlatformGame.Characters
 {
     public class Samurai : IGameObject
     {
         private Texture2D texture;
-        private Rectangle _frameSelector;
         private int move_X = 0;
+        Animation animation;
 
         public Samurai(Texture2D texture)
         {
             this.texture = texture;
-            _frameSelector = new Rectangle(0, 0, 0, 22);
+            animation = new Animation();
+            animation.GetFramesFromTextureProperties(texture.Width, texture.Height, 3, 1);
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(texture, new Vector2(0, 0), _frameSelector, Color.White, 0, new Vector2(0, 0), 4.0f, SpriteEffects.None, 0);
+            spriteBatch.Draw(texture, new Vector2(0, 0), animation.CurrentFrame.SourceRectangle, Color.White, 0, new Vector2(0, 0), 4.0f, SpriteEffects.None, 0);
         }
 
         public void Update()
         {
-            move_X += 30;
-            if (move_X > 60)
-            {
-                move_X = 0;
-            }
-            _frameSelector.X = move_X;
+            animation.Update();
         }
     }
 }
