a1bf96d250a05d007cefc3a97cdfea54acff7e48
Jordi Timmers <timmersjordi@outlook.com>
2022-10-29 18:08:36 +0200

    AnimationPool refactor

---
+++ 2DPlatformGame/2DPlatformGame/Animations/AnimationPool.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using DPlatformGame.Input;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace DPlatformGame.Animations
+{
+    public class AnimationPool
+    {
+        public List<Animation> AnimationList = new List<Animation>();
+        public List<Texture2D> Texturelist = new List<Texture2D>();
+        public SpriteEffects effect;
+
+        private Vector2 position;
+        private Vector2 speed;
+        private Rectangle currentFrame;
+        private Texture2D currentTexture;
+        private float scale = 4.0f;
+
+        public AnimationPool()
+        {
+            effect = SpriteEffects.None;
+            position = new Vector2(0, 0);
+            speed = new Vector2(5, 0);
+        }
+
+        public void AddAnimation(Animation a, Texture2D t)
+        {
+            this.AnimationList.Add(a);
+            this.Texturelist.Add(t);
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            KeyboardReader k = new KeyboardReader();
+            var direction = k.ReadInput();
+            direction *= speed;
+            position += direction;
+
+            CheckForFlip(direction);
+
+            //chosing between animations based on movement
+            if (direction.X == 0)
+            {
+                AnimationList[0].Update(gameTime);
+                currentTexture = Texturelist[0];
+                currentFrame = AnimationList[0].CurrentFrame.SourceRectangle;
+            }
+            else
+            {
+                AnimationList[1].Update(gameTime);
+                currentTexture = Texturelist[1];
+                currentFrame = AnimationList[1].CurrentFrame.SourceRectangle;
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(this.currentTexture, this.position, this.currentFrame, Color.White, 0, new Vector2(0, 0), scale, effect, 0);
+        }
+
+        public void CheckForFlip(Vector2 direction)
+        {
+            if (direction.X > 0)
+            {
+                effect = SpriteEffects.None;
+            }
+            else if (direction.X < 0)
+            {
+                effect = SpriteEffects.FlipHorizontally;
+            }
+        }
+    }
+}
+
--- 2DPlatformGame/2DPlatformGame/Characters/Samurai.cs
+++ 2DPlatformGame/2DPlatformGame/Characters/Samurai.cs
@@ -10,94 +10,27 @@ namespace DPlatformGame.Characters
 {
     public class Samurai : IGameObject
     {
-        private Texture2D idleTexture;
-        private Texture2D moveTexture;
-        private Vector2 position;
-        private Vector2 speed;
-        float scale = 4.0f;
-        Animation idleAnimation;
-        Animation moveAnimation;
-        SpriteEffects effect;
-
-        Rectangle currentFrame;
-        Texture2D currentTexture;
+        AnimationPool pool;
 
         public Samurai(Texture2D idleTexture, Texture2D moveTexture)
         {
-            //create spriteeffect
-            effect = SpriteEffects.None;
 
-            //get textures from constructor
-            this.idleTexture = idleTexture;
-            this.moveTexture = moveTexture;
-
-            //create idle animation (this is with the texture you receive from the constructor parameter)
-            this.idleAnimation = new Animation();
-            this.idleAnimation.GetFramesFromTextureProperties(idleTexture.Width, idleTexture.Height, 3, 1);
-
-            //create move animation
-            this.moveAnimation = new Animation();
-            this.moveAnimation.GetFramesFromTextureProperties(moveTexture.Width, moveTexture.Height, 12, 1);
-
-
-            position = new Vector2(0,0);
-            speed = new Vector2(5,0);
+            //create animation pool
+            pool = new AnimationPool();
+            pool.AddAnimation(new Animation(), idleTexture); //idle
+            pool.AnimationList[0].GetFramesFromTextureProperties(idleTexture.Width, idleTexture.Height, 3, 1); //idle
+            pool.AddAnimation(new Animation(), moveTexture); //move
+            pool.AnimationList[1].GetFramesFromTextureProperties(moveTexture.Width, moveTexture.Height, 12, 1); //move
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(this.currentTexture, this.position, this.currentFrame, Color.White, 0, new Vector2(0, 0), scale, effect, 0); //this.moveAnimation.CurrentFrame.SourceRectangle
+            pool.Draw(spriteBatch);
         }
 
         public void Update(GameTime gameTime)
         {
-            //input handling
-            KeyboardReader k = new KeyboardReader();
-            var direction = k.ReadInput();
-            CheckForFlip(direction);
-            direction *= speed;
-            position += direction;
-
-            //Animation decider
-            if(direction.X == 0)
-            {
-                idleAnimation.Update(gameTime);
-                currentTexture = idleTexture;
-                currentFrame = idleAnimation.CurrentFrame.SourceRectangle;
-            }
-            else
-            {
-                moveAnimation.Update(gameTime);
-                currentTexture = moveTexture;
-                currentFrame = moveAnimation.CurrentFrame.SourceRectangle;
-            }
-        }
-
-        private void CheckForFlip(Vector2 direction)
-        {
-            if(direction.X > 0)
-            {
-                effect = SpriteEffects.None;
-            }
-            else if (direction.X < 0)
-            {
-                effect = SpriteEffects.FlipHorizontally;
-            }
-        }
-
-        private void Move()
-        {
-            position += speed;
-            if (position.X > 1280 - ((moveTexture.Width/12)*scale)
-                || position.X < 0)
-            {
-                speed.X *= -1;
-            }
-            if (position.Y > 720 - ((moveTexture.Height*1)*scale)
-                || position.Y < 0)
-            {
-                speed.Y *= -1;
-            }
+            pool.Update(gameTime);
         }
     }
 }
